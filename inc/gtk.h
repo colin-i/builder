b1a74d3bc9b1e3e78acba0680a70475bf44af9e0
Costin <costin.botescu@gmail.com>
2020-09-20 19:58:22 +0300

    files

---
+++ inc/gtk.h
@@ -0,0 +1,36 @@
+
+typedef char gchar;
+typedef unsigned long gulong;
+typedef void* gpointer;
+
+typedef struct _GApplication GApplication;
+typedef struct _GtkApplication GtkApplication;
+typedef struct _GtkBox GtkBox;
+typedef struct _GtkWidget GtkWidget;
+typedef struct _GtkWindow GtkWindow;
+
+typedef void (*GCallback) (void);
+typedef struct _GClosure GClosure;
+typedef void (*GClosureNotify) (gpointer data,
+      GClosure *closure);
+
+typedef enum{G_APPLICATION_FLAGS_NONE}
+ GApplicationFlags;
+typedef enum{G_CONNECT_SWAPPED = 1 << 1}
+ GConnectFlags;
+typedef enum{GTK_ORIENTATION_HORIZONTAL,GTK_ORIENTATION_VERTICAL}
+ GtkOrientation;
+
+#define G_CALLBACK(f) ((GCallback) (f))
+
+int g_application_run (GApplication *application,int argc,char **argv);
+void g_object_unref (gpointer object);
+gulong g_signal_connect_data (gpointer instance,const gchar *detailed_signal,GCallback c_handler,gpointer data,GClosureNotify destroy_data,GConnectFlags con);
+GtkApplication * gtk_application_new (const gchar *application_id, GApplicationFlags flags);
+GtkWidget * gtk_application_window_new (GtkApplication *application);
+void gtk_box_append (GtkBox *box, GtkWidget *child);
+GtkWidget* gtk_box_new (GtkOrientation orientation,int spacing);
+GtkWidget* gtk_button_new (void);
+GtkWidget* gtk_label_new (const char *str);
+void gtk_widget_show (GtkWidget *widget);
+void gtk_window_set_child (GtkWindow *window,GtkWidget *child);
